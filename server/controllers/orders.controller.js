b8016522a3e99d0003b0189c2060c9b8c02e7de2
J-Gilberg <85514341+J-Gilberg@users.noreply.github.com>
2021-11-03 08:29:36 -0500

    server update

--- server/controllers/orders.controller.js
+++ server/controllers/orders.controller.js
@@ -1,77 +1,40 @@
-const UserModel = require('../models/user.model');
+const UserModel = require('../models/orders.model');
 const HttpException = require('../utils/HttpException.utils');
-const { validationResult } = require('express-validator');
-const bcrypt = require('bcryptjs');
+// const { validationResult } = require('express-validator');
+// const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
 dotenv.config();
 
-/******************************************************************************
- *                              User Controller
- ******************************************************************************/
-class UserController {
-    getAllUsers = async (req, res, next) => {
-        let userList = await UserModel.find();
-        if (!userList.length) {
-            throw new HttpException(404, 'Users not found');
+class OrdersConroller {
+    getAllOrders = async (req, res, next) => {
+        let ordersList = await OrderModel.find();
+        if (ordersList.length) {
+            throw new HttpException(404, 'Orders not found');
         }
-
-        userList = userList.map(user => {
-            const { password, ...userWithoutPassword } = user;
-            return userWithoutPassword;
-        });
-
-        res.send(userList);
+        res.send(ordersList);
     };
 
-    getUserById = async (req, res, next) => {
-        const user = await UserModel.findOne({ id: req.params.id });
-        if (!user) {
-            throw new HttpException(404, 'User not found');
+    getOrderByProductId = async (req, res, next) => {
+        const ordersList = await OrderModel.findAll({ product_id: req.params.product_id });
+        if (ordersList.length) {
+            throw new HttpException(404, 'Orders not found');
         }
+        res.send(ordersList);
+    };S
 
-        const { password, ...userWithoutPassword } = user;
-
-        res.send(userWithoutPassword);
-    };
-
-    getUserByuserName = async (req, res, next) => {
-        const user = await UserModel.findOne({ username: req.params.username });
-        if (!user) {
-            throw new HttpException(404, 'User not found');
-        }
-
-        const { password, ...userWithoutPassword } = user;
-
-        res.send(userWithoutPassword);
-    };
-
-    getCurrentUser = async (req, res, next) => {
-        const { password, ...userWithoutPassword } = req.currentUser;
-
-        res.send(userWithoutPassword);
-    };
-
-    createUser = async (req, res, next) => {
-        this.checkValidation(req);
-
-        await this.hashPassword(req);
-
+    createOrder = async (req, res, next) => {
         const result = await UserModel.create(req.body);
 
         if (!result) {
             throw new HttpException(500, 'Something went wrong');
         }
 
-        res.status(201).send('User was created!');
+        res.status(201).send('Order was created!');
     };
 
-    updateUser = async (req, res, next) => {
-        this.checkValidation(req);
-
-        await this.hashPassword(req);
-
-        const { confirm_password, ...restOfUpdates } = req.body;
+    updateOrder = async (req, res, next) => {
+        const { ...restOfUpdates } = req.body;
 
         // do the update query and get the result
         // it can be partial edit
@@ -83,66 +46,153 @@ class UserController {
 
         const { affectedRows, changedRows, info } = result;
 
-        const message = !affectedRows ? 'User not found' :
-            affectedRows && changedRows ? 'User updated successfully' : 'Updated faild';
+        const message = !affectedRows ? 'Order not found' :
+            affectedRows && changedRows ? 'Order updated successfully' : 'Updated faild';
 
         res.send({ message, info });
     };
-
-    deleteUser = async (req, res, next) => {
-        const result = await UserModel.delete(req.params.id);
-        if (!result) {
-            throw new HttpException(404, 'User not found');
-        }
-        res.send('User has been deleted');
-    };
-
-    userLogin = async (req, res, next) => {
-        this.checkValidation(req);
-
-        const { email, password: pass } = req.body;
-
-        const user = await UserModel.findOne({ email });
-
-        if (!user) {
-            throw new HttpException(401, 'Unable to login!');
-        }
-
-        const isMatch = await bcrypt.compare(pass, user.password);
-
-        if (!isMatch) {
-            throw new HttpException(401, 'Incorrect password!');
-        }
-
-        // user matched!
-        const secretKey = process.env.SECRET_JWT || "";
-        const token = jwt.sign({ user_id: user.id.toString() }, secretKey, {
-            expiresIn: '24h'
-        });
-
-        const { password, ...userWithoutPassword } = user;
-
-        res.send({ ...userWithoutPassword, token });
-    };
-
-    checkValidation = (req) => {
-        const errors = validationResult(req)
-        if (!errors.isEmpty()) {
-            throw new HttpException(400, 'Validation faild', errors);
-        }
-    }
-
-    // hash password if it exists
-    hashPassword = async (req) => {
-        if (req.body.password) {
-            req.body.password = await bcrypt.hash(req.body.password, 8);
-        }
-    }
 }
+
+
+
+
+/******************************************************************************
+ *                              User Controller
+ ******************************************************************************/
+// class UserController {
+//     getAllUsers = async (req, res, next) => {
+//         let userList = await UserModel.find();
+//         if (!userList.length) {
+//             throw new HttpException(404, 'Users not found');
+//         }
+
+//         userList = userList.map(user => {
+//             const { password, ...userWithoutPassword } = user;
+//             return userWithoutPassword;
+//         });
+
+//         res.send(userList);
+//     };
+
+//     getUserById = async (req, res, next) => {
+//         const user = await UserModel.findOne({ id: req.params.id });
+//         if (!user) {
+//             throw new HttpException(404, 'User not found');
+//         }
+
+//         const { password, ...userWithoutPassword } = user;
+
+//         res.send(userWithoutPassword);
+//     };
+
+//     getUserByuserName = async (req, res, next) => {
+//         const user = await UserModel.findOne({ username: req.params.username });
+//         if (!user) {
+//             throw new HttpException(404, 'User not found');
+//         }
+
+//         const { password, ...userWithoutPassword } = user;
+
+//         res.send(userWithoutPassword);
+//     };
+
+//     getCurrentUser = async (req, res, next) => {
+//         const { password, ...userWithoutPassword } = req.currentUser;
+
+//         res.send(userWithoutPassword);
+//     };
+
+//     createUser = async (req, res, next) => {
+//         this.checkValidation(req);
+
+//         await this.hashPassword(req);
+
+//         const result = await UserModel.create(req.body);
+
+//         if (!result) {
+//             throw new HttpException(500, 'Something went wrong');
+//         }
+
+//         res.status(201).send('User was created!');
+//     };
+
+//     updateUser = async (req, res, next) => {
+//         this.checkValidation(req);
+
+//         await this.hashPassword(req);
+
+//         const { confirm_password, ...restOfUpdates } = req.body;
+
+//         // do the update query and get the result
+//         // it can be partial edit
+//         const result = await UserModel.update(restOfUpdates, req.params.id);
+
+//         if (!result) {
+//             throw new HttpException(404, 'Something went wrong');
+//         }
+
+//         const { affectedRows, changedRows, info } = result;
+
+//         const message = !affectedRows ? 'User not found' :
+//             affectedRows && changedRows ? 'User updated successfully' : 'Updated faild';
+
+//         res.send({ message, info });
+//     };
+
+//     deleteUser = async (req, res, next) => {
+//         const result = await UserModel.delete(req.params.id);
+//         if (!result) {
+//             throw new HttpException(404, 'User not found');
+//         }
+//         res.send('User has been deleted');
+//     };
+
+//     userLogin = async (req, res, next) => {
+//         this.checkValidation(req);
+
+//         const { email, password: pass } = req.body;
+
+//         const user = await UserModel.findOne({ email });
+
+//         if (!user) {
+//             throw new HttpException(401, 'Unable to login!');
+//         }
+
+//         const isMatch = await bcrypt.compare(pass, user.password);
+
+//         if (!isMatch) {
+//             throw new HttpException(401, 'Incorrect password!');
+//         }
+
+//         // user matched!
+//         const secretKey = process.env.SECRET_JWT || "";
+//         const token = jwt.sign({ user_id: user.id.toString() }, secretKey, {
+//             expiresIn: '24h'
+//         });
+
+//         const { password, ...userWithoutPassword } = user;
+
+//         res.send({ ...userWithoutPassword, token });
+//     };
+
+//     checkValidation = (req) => {
+//         const errors = validationResult(req)
+//         if (!errors.isEmpty()) {
+//             throw new HttpException(400, 'Validation faild', errors);
+//         }
+//     }
+
+//     // hash password if it exists
+//     hashPassword = async (req) => {
+//         if (req.body.password) {
+//             req.body.password = await bcrypt.hash(req.body.password, 8);
+//         }
+//     }
+// }
 
 
 
 /******************************************************************************
  *                               Export
  ******************************************************************************/
-module.exports = new UserController;
+module.exports = new OrdersController;
--- server/models/orders.model.js
+++ server/models/orders.model.js
@@ -1,8 +1,8 @@
 const query = require('../db/db-connection');
 const { multipleColumnSet } = require('../utils/common.utils');
-const Role = require('../utils/userRoles.utils');
-class UserModel {
-    tableName = 'user';
+// const Role = require('../utils/userRoles.utils');
+class OrderModel {
+    tableName = 'orders';
 
     find = async (params = {}) => {
         let sql = `SELECT * FROM ${this.tableName}`;
@@ -29,7 +29,19 @@ class UserModel {
         return result[0];
     }
 
-    create = async ({ username, password, first_name, last_name, email, role = Role.SuperUser, age = 0 }) => {
+    findAll = async (params) => {
+        const { columnSet, values } = multipleColumnSet(params)
+
+        const sql = `SELECT * FROM ${this.tableName}
+        WHERE ${columnSet}`;
+
+        const result = await query(sql, [...values]);
+
+        // return back the first row (user)
+        return result;
+    }
+
+    create = async ({ username, password, first_name, last_name, email, age = 0 }) => {
         const sql = `INSERT INTO ${this.tableName}
         (username, password, first_name, last_name, email, role, age) VALUES (?,?,?,?,?,?,?)`;
 
@@ -59,4 +71,62 @@ class UserModel {
     }
 }
 
-module.exports = new UserModel;
+module.exports = new OrderModel;
+
+// class UserModel {
+//     tableName = 'user';
+
+//     find = async (params = {}) => {
+//         let sql = `SELECT * FROM ${this.tableName}`;
+
+//         if (!Object.keys(params).length) {
+//             return await query(sql);
+//         }
+
+//         const { columnSet, values } = multipleColumnSet(params)
+//         sql += ` WHERE ${columnSet}`;
+
+//         return await query(sql, [...values]);
+//     }
+
+//     findOne = async (params) => {
+//         const { columnSet, values } = multipleColumnSet(params)
+
+//         const sql = `SELECT * FROM ${this.tableName}
+//         WHERE ${columnSet}`;
+
+//         const result = await query(sql, [...values]);
+
+//         // return back the first row (user)
+//         return result[0];
+//     }
+
+//     create = async ({ username, password, first_name, last_name, email, role = Role.SuperUser, age = 0 }) => {
+//         const sql = `INSERT INTO ${this.tableName}
+//         (username, password, first_name, last_name, email, role, age) VALUES (?,?,?,?,?,?,?)`;
+
+//         const result = await query(sql, [username, password, first_name, last_name, email, role, age]);
+//         const affectedRows = result ? result.affectedRows : 0;
+
+//         return affectedRows;
+//     }
+
+//     update = async (params, id) => {
+//         const { columnSet, values } = multipleColumnSet(params)
+
+//         const sql = `UPDATE user SET ${columnSet} WHERE id = ?`;
+
+//         const result = await query(sql, [...values, id]);
+
+//         return result;
+//     }
+
+//     delete = async (id) => {
+//         const sql = `DELETE FROM ${this.tableName}
+//         WHERE id = ?`;
+//         const result = await query(sql, [id]);
+//         const affectedRows = result ? result.affectedRows : 0;
+
+//         return affectedRows;
+//     }
+// }
