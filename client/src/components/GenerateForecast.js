6100bcd8828377417de114ccb0ab17e633e15778
J-Gilberg <85514341+J-Gilberg@users.noreply.github.com>
2021-11-09 14:01:35 -0600

    Finished Cubic Regression Equation

--- client/src/components/GenerateForecast.js
+++ client/src/components/GenerateForecast.js
@@ -46,51 +46,151 @@ export const generateCREquation = (inputs) => {
             .then((res) => {
                 console.log(Object.keys(res));
                 data = res.data;
-                let a = 0;
-                let b = 0;
-                let c = 0;
-                let d = 0;
-                let X = [];
-                let XTX = [[], [], [], []];
-                let tempSum = [];
-                let outputs = [];
+                let X = []; //Matrix of 1,x,x2,x3
+                let y = 0; //y calc bases of order_unix start moving y to = 0
+                let XTX = [[], [], [], []]; //result of XT * X (XT is the transpose of X row = col, col = row)
+                let XTY = [] //result of XT * Y (XT is the transpose of X row = col, col = row)
+                let D = 0; //determinant
+                let DD = 0; //1/D
+                let MM = []; //matrix of minors
+                let XTXinv = [] //Inverse of XTX
+                let temp = [];
+                let i = 0;
+                let j = 0;
+                let coefficients = [0, 0, 0, 0];
+
                 data.forEach((dp, idx) => {
                     X.push([1, dp.quantity, dp.xsqr, dp.xcube])
                 });
                 console.log(X);
-                for (let i = 0; i < 4; ++i) {
+                for (let i = 0; i < 4; i = i + 3) {
                     for (let j = 0; j < X.length; ++j) {
-                        if (!tempSum.length) {
-                            tempSum.push(X[j][i] * X[j][i]);
-                            if (i + 1 < 4) tempSum.push(X[j][i] * X[j][i + 1]);
-                            if (i + 2 < 4) tempSum.push(X[j][i] * X[j][i + 2]);
-                            if (i + 3 < 4) tempSum.push(X[j][i] * X[j][i + 3]);
+                        if (i === 0) {
+                            if (!temp.length) {
+                                temp.push(X[j][i] * X[j][0]);
+                                temp.push(X[j][i] * X[j][1]);
+                                temp.push(X[j][i] * X[j][2]);
+                                temp.push(X[j][i] * X[j][3]);
+                            } else {
+                                temp[0] += X[j][i] * X[j][0];
+                                temp[1] += X[j][i] * X[j][1];
+                                temp[2] += X[j][i] * X[j][2];
+                                temp[3] += X[j][i] * X[j][3];
+                            }
                         } else {
-                            tempSum[i] += X[j][i] * X[j][i];
-                            if (i + 1 < 4) tempSum[i + 1] += X[j][i] * X[j][i + 1];
-                            if (i + 2 < 4) tempSum[i + 2] += X[j][i] * X[j][i + 2];
-                            if (i + 3 < 4) tempSum[i + 3] += X[j][i] * X[j][i + 3];
+                            if (!temp.length) {
+                                temp.push(X[j][i] * X[j][1]);
+                                temp.push(X[j][i] * X[j][2]);
+                                temp.push(X[j][i] * X[j][3]);
+                            } else {
+                                temp[0] += X[j][i] * X[j][1];
+                                temp[1] += X[j][i] * X[j][2];
+                                temp[2] += X[j][i] * X[j][3];
+                            }
                         }
                     }
-                    console.log(tempSum)
-                    XTX[i] = [...XTX[i], ...tempSum];
-                    if (i + 1 < 4) XTX[i + 1] = [...XTX[i + 1], ...tempSum.slice(i + 1, tempSum.length)];
-                    if (i + 2 < 4) XTX[i + 2] = [...XTX[i + 2], ...tempSum.slice(i + 2, tempSum.length)];
-                    if (i + 3 < 4) XTX[i + 3] = [...XTX[i + 3], ...tempSum.slice(i + 3, tempSum.length)];
-                    tempSum = []
+                    if (i === 0) {
+                        XTX[i] = [...XTX[i], ...temp];
+                        XTX[i + 1] = [...XTX[i + 1], ...temp.slice(i + 1, temp.length)];
+                        XTX[i + 2] = [...XTX[i + 2], ...temp.slice(i + 2, temp.length)];
+                        XTX[i + 3] = [...XTX[i + 3], ...temp.slice(i + 3, temp.length)];
+                    } else {
+                        XTX[i] = [...XTX[i], ...temp];
+                        XTX[2] = [...XTX[2], ...temp.slice(0, 2)];
+                        XTX[1] = [...XTX[1], ...temp.slice(0, 1)];
+                    }
+                    temp = [];
                 }
                 console.log(XTX);
-                console.log(`a: ${a}`);
-                console.log(`b: ${b}`);
-                console.log(`a: ${c}`);
-                console.log(`b: ${d}`);
-                outputs.push({ a: a, b: b, c: c, d: d });
+                //determinant of 3X3 matrix |A| = a(ei − fh) − b(di − fg) + c(dh − eg)
+                //A =   abc
+                //      def
+                //      ghi
 
+
+                XTX.forEach((row, idx) => {
+                    MM.push([]);
+                    XTX[idx].forEach((dp, jdx) => {
+
+                        i = 0;
+                        j = 0;
+                        while (i < XTX.length) {
+                            if (i === idx) {
+                                ++i;
+                            }
+                            if (j === jdx) {
+                                ++j;
+                            }
+                            if (j < XTX.length && i < XTX.length) {
+                                temp.push(XTX[i][j]);
+                                // console.log(`temp ${temp}`);
+                                // console.log(`j ${j}`);
+                                // console.log(`i ${i}`);
+                                ++j;
+                            } else {
+                                j = 0;
+                                ++i;
+                            }
+                        }
+                        MM[idx].push((temp[0] * (temp[4] * temp[8] - temp[5] * temp[7]) - temp[1] * (temp[3] * temp[8] - temp[5] * temp[6]) + temp[2] * (temp[3] * temp[7] - temp[4] * temp[6])) * ((idx + jdx) % 2 === 0 ? 1 : -1));
+                        // console.log(`idx: ${idx}, jdx: ${jdx}`)
+                        temp = [];
+                    })
+                })
+                // Adjugate (also called Adjoint) swap col/row idx
+                i = 0;
+                j = 0;
+                while (i < MM.length) {
+                    [MM[i][j], MM[j][i]] = [MM[j][i], MM[i][j]];
+                    if (j + 1 >= MM.length) {
+                        ++i;
+                        j = i;
+                    } else {
+                        ++j;
+                    }
+                }
+                console.log(MM);
+                // //Determinant calc hard coded for now will work this into a algo later
+                D = XTX[0][0] * XTX[1][1] * XTX[2][2] * XTX[3][3] - XTX[0][1] * XTX[1][0] * XTX[2][2] * XTX[3][3] + XTX[0][2] * XTX[1][0] * XTX[2][1] * XTX[3][3]
+                    - XTX[0][0] * XTX[1][2] * XTX[2][1] * XTX[3][3] + XTX[0][1] * XTX[1][2] * XTX[2][0] * XTX[3][3] - XTX[0][2] * XTX[1][1] * XTX[2][0] * XTX[3][3]
+                    + XTX[0][2] * XTX[1][1] * XTX[2][3] * XTX[3][0] - XTX[0][1] * XTX[1][2] * XTX[2][3] * XTX[3][0] + XTX[0][3] * XTX[1][2] * XTX[2][1] * XTX[3][0]
+                    - XTX[0][2] * XTX[1][3] * XTX[2][1] * XTX[3][0] + XTX[0][1] * XTX[1][3] * XTX[2][2] * XTX[3][0] - XTX[0][3] * XTX[1][1] * XTX[2][2] * XTX[3][0]
+                    + XTX[0][3] * XTX[1][0] * XTX[2][2] * XTX[3][1] - XTX[0][0] * XTX[1][3] * XTX[2][2] * XTX[3][1] + XTX[0][2] * XTX[1][3] * XTX[2][0] * XTX[3][1]
+                    - XTX[0][3] * XTX[1][2] * XTX[2][0] * XTX[3][1] + XTX[0][0] * XTX[1][2] * XTX[2][3] * XTX[3][1] - XTX[0][2] * XTX[1][0] * XTX[2][3] * XTX[3][1]
+                    + XTX[0][1] * XTX[1][0] * XTX[2][3] * XTX[3][2] - XTX[0][0] * XTX[1][1] * XTX[2][3] * XTX[3][2] + XTX[0][3] * XTX[1][1] * XTX[2][0] * XTX[3][2]
+                    - XTX[0][1] * XTX[1][3] * XTX[2][0] * XTX[3][2] + XTX[0][0] * XTX[1][3] * XTX[2][1] * XTX[3][2] - XTX[0][3] * XTX[1][0] * XTX[2][1] * XTX[3][2];
+                console.log(D);
+                // //because coordinates start at 0 negatives are at evens not odds.
+                DD = 1 / D;
+                MM.forEach((i, idx) => {
+                    XTXinv.push([])
+                    MM[idx].forEach((dp, jdx) => {
+                        XTXinv[idx].push(DD * dp);
+                    })
+                })
+                //XT*y
+                console.log(XTXinv);
+                XTXinv.forEach((row, idx) => {
+                    XTY.push([])
+                    X.forEach((dp,jdx)=>{
+                        XTY[idx].push((XTXinv[idx][0] * X[jdx][0]) + (XTXinv[idx][1] * X[jdx][1]) + (XTXinv[idx][2] * X[jdx][2]) + (XTXinv[idx][3] * X[jdx][3]));
+                    })
+                })
+                console.log(XTY);
+                data.forEach((dp,idx)=>{
+                    y = dp.order_unix - data[0].order_unix;
+                    console.log(y)
+                    coefficients[0] += XTY[0][idx] * y;
+                    coefficients[1] += XTY[1][idx] * y;
+                    coefficients[2] += XTY[2][idx] * y;
+                    coefficients[3] += XTY[3][idx] * y;
+                })
+                console.log(coefficients);
+                return coefficients;
             })
             .catch((error) => {
                 console.log(error);
             });
-
     }
 
     //β = (XTX)-1XTy
--- server/models/calcs.model.js
+++ server/models/calcs.model.js
@@ -6,7 +6,7 @@ class CalcsModel {
 
     getLRCalcs = async (body) => {
         const { columnSet, values } = multipleColumnSetWithDateRange(body);
-        let sql = `SELECT product_id, SUM(quantity) xsum, SUM(order_unix) ysum, SUM(xy) xysum, SUM(xsqr) xsqrsum, SUM(ysqr) ysqrsum, ((UNIX_TIMESTAMP(MAX(order_date))-1262325600)/86400) - ((UNIX_TIMESTAMP(MIN(order_date))-1262325600)/86400) n FROM ${this.tableName} WHERE ${columnSet}`;
+        let sql = `SELECT product_id, SUM(quantity) xsum, SUM(order_unix) ysum, SUM(xy) xysum, SUM(xsqr) xsqrsum, SUM(ysqr) ysqrsum, ((UNIX_TIMESTAMP(MAX(order_date))-1262325600)/86400) - ((UNIX_TIMESTAMP(MIN(order_date))-1262325600)/86400) n FROM ${this.tableName} WHERE ${columnSet} ORDER BY order_date ASC`;
         console.log(sql);
         console.log(values);
         return await query(sql, [...values]);
@@ -15,7 +15,7 @@ class CalcsModel {
     getCRCalcs = async (body) => {
         console.log(`body ${Object.keys(body)}`)
         const { columnSet, values } = multipleColumnSetWithDateRange(body);
-        let sql = `SELECT * FROM ${this.tableName} WHERE ${columnSet}`;
+        let sql = `SELECT * FROM ${this.tableName} WHERE ${columnSet} ORDER BY order_date ASC`;
         console.log(sql);
         console.log(values);
         return await query(sql, [...values]);
