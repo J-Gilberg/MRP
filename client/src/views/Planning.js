a74876a905505b1c1a67c480bca63dcaa81bbbaf
J-Gilberg <85514341+J-Gilberg@users.noreply.github.com>
2021-12-15 16:46:13 -0600

    building a better way to populate planning data

--- client/src/views/Planning.js
+++ client/src/views/Planning.js
@@ -1,32 +1,31 @@
 import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router';
-import {generateCREquation} from './components/GenerateForecast';
+import { generateCREquation } from './components/GenerateForecast';
 
 
 const Planning = () => {
     const [productId, setProductId] = useState('');
     const [startDate, setStartDate] = useState('');
     const [endDate, setEndDate] = useState('');
-    const [productData, setProductData] = useState([]);
+    const [dateRange, setDateRange] = useState([]);
+    const [productData, setProductData] = useState({}); //{order: [], purchase: [], planned_purhcase: [], forecast: []}
     const [equation, setEquation] = useState();
 
-    const [error, setError] = useState();
+    const [error, setError] = useState('');
     useEffect(() => {
     }, []);
 
 
     const getData = (e) => {
-        if(!productId && !startDate && !endDate){
+        if (!productId && !startDate && !endDate) {
             //need to add error logic.
+            
             setEquation(generateCREquation({ start: startDate, end: endDate, product_id: productId }))
-            axios.get(`http://localhost:3331/api/orders/one`, { headers: inputs })
-                .then((res) => {setProductData(res)})
-                .catch((err) => {setError(err)});
+            axios.get(`http://localhost:3331/api/planning/one`, { headers: inputs })
+                .then((res) => { setProductData(res) })
+                .catch((err) => { setError(err) });
         }
-    
     }
-
-
     return (
         <div>
             <h1>Planning</h1>
@@ -34,31 +33,46 @@ const Planning = () => {
                 <div>
                     <h2>Enter Product Id</h2>
                     <div>
+                        <h1>{error}</h1>
                         <form onSubmit={getData}>
-                            <input type="number" name='productId' onChange={(e)=>{setProductId(e.target.productId)}}/>
-                            <input type="date" name='start' onChange={(e)=>{setStartDate(e.target.start)}}/>
-                            <input type="date" name='end' onChange={(e)=>{setEndDate(e.target.end)}}/>
-                            <input type="submit" name='submit' value='Submit'/>
+                            <input type="number" name='productId' onChange={(e) => { setProductId(e.target.productId) }} />
+                            <input type="date" name='start' onChange={(e) => { setStartDate(e.target.start) }} />
+                            <input type="date" name='end' onChange={(e) => { setEndDate(e.target.end) }} />
+                            <input type="submit" name='submit' value='Submit' />
                         </form>
                     </div>
                 </div>
-                
-                <div id='actualSales' className='cellRows'>
-                    productData.map(()
+                <div id='date' className='cellRows'>
+                    {dateRange.map((item, idx) => {
+                        <h2>{item.toString()}</h2>
+
+                    })}
+                    <div id='actualSales' className='cellRows'>
+                        {productData.forecast.map((item, idx) => {
+
+                        })}
+                    </div>
+
+                    <div id='plannedSales' className='cellRows'>
+                        {productData.forecast.map((item, idx) => {
+
+                        })}
+                    </div>
+
+                    <div id='actualPurchases' className='cellRows'>
+                        {productData.forecast.map((item, idx) => {
+
+                        })}
+                    </div>
+
+                    <div id='plannedPurchases' className='cellRows'>
+                        {productData.forecast.map((item, idx) => {
+
+                        })}
+                    </div>
+                    
                 </div>
 
-                <div id='plannedSales' className='cellRows'>
-
-                </div>
-
-                <div id='actualPurchases' className='cellRows'>
-
-                </div>
-
-                <div id='plannedPurchases' className='cellRows'>
-
-                </div>
-                )
             </div>
         </div>
     )
--- server/models/planning.model.js
+++ server/models/planning.model.js
@@ -2,22 +2,65 @@ const query = require('../config/conn');
 const { multipleColumnSet } = require('../utils/common.utils');
 // const Role = require('../utils/userRoles.utils');
 class PlanningModel {
-    tableName = 'order_calcs';
 
     getPlanningData = async (body) => {
+        let data =  {order: [], purchase: [], planned_purhcase: [], forecast: []}
         console.log(`body ${Object.keys(body)}`)
         const { columnSet, values } = multipleColumnSetWithDateRange(body);
-        let sql = `SELECT * 
-        FROM ${this.tableName} o 
-        JOIN purchases p ON o.order_unix = p.purchase_unix
-        JOIN planned_purchases pp ON o.order_unix = pp.planned_purchase_unix
-        JOIN forecasts f ON o.order_unix = f.forecast_unix 
+        let sql = `SELECT o.product_id
+        ,o.order_date
+        ,o.order_day
+        ,o.order_week
+        ,o.order_month
+        ,o.order_year
+        ,o.order_unix
+        ,o.quantity	order_quantity
+        FROM order_calcs o 
         WHERE ${columnSet} 
-        ORDER BY o.order_date ASC`; // get more data probably need to specify the columns to reduce data received
-        console.log(sql);
-        console.log(values);
-        return await query(sql, [...values]);
+        ORDER BY o.order_date ASC`; 
+        data.order = await query(sql, [...values]);
+
+        sql = `SELECT p.product_id
+        ,p.purchase_date
+        ,p.purchase_day
+        ,p.purchase_week
+        ,p.purchase_month
+        ,p.purchase_year
+        ,p.purchase_unix
+        ,p.quantity	purchase_quantity
+        FROM purchases p
+        WHERE ${columnSet} 
+        ORDER BY p.purchase_date ASC`;
+        data.purchase = await query(sql, [...values]);
+
+        sql = `SELECT p.product_id
+        ,p.planned_purchase_date
+        ,p.planned_purchase_day
+        ,p.planned_purchase_week
+        ,p.planned_purchase_month
+        ,p.planned_purchase_year
+        ,p.planned_purchase_unix
+        ,p.quantity	planned_purchase_quantity
+        FROM planned_purchase p 
+        WHERE ${columnSet} 
+        ORDER BY p.planned_purhcase_date ASC`;
+        data.planned_purhcase = await query(sql, [...values]);
+
+        sql = `SELECT f.product_id
+        ,f.forecast_date
+        ,f.forecast_day
+        ,f.forecast_week
+        ,f.forecast_month
+        ,f.forecast_year
+        ,f.forecast_unix
+        ,f.quantity	forecast_quantity
+        FROM forecasts f  
+        WHERE ${columnSet} 
+        ORDER BY f.forecast_date ASC`;
+        data.planned_purhcase = await query(sql, [...values]);
+        return data;
     }
 }
-
 module.exports = new PlanningModel;
+
+
